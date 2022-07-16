cd72a23e097d2c174138f359abde6b30d714cd4c
Flexxmesoft <109343897+Flexxmesoft@users.noreply.github.com>
2022-07-16 08:15:14 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+$ heroku config:set DISABLE_COLLECTSTATIC=1
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gui_config.settings')
